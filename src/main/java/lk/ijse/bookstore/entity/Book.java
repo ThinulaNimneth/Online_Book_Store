9f9a3ae5ed3670e163dca5c5504ed3b85850933b
ThinulaNimneth <nimnethnetwork@gmail.com>
2026-08-18 11:08:57 +0530

    update entity

--- src/main/java/lk/ijse/bookstore/entity/Book.java
+++ src/main/java/lk/ijse/bookstore/entity/Book.java
@@ -1,7 +1,6 @@
 package lk.ijse.bookstore.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,4 +13,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+
+    @Column(nullable = false,length = 300)
+    private String title;
+
+    @Column(unique = true, length = 50)
+    private String isbn;
+
+    @Column(length = 500)
+    private String description;
+
 }
