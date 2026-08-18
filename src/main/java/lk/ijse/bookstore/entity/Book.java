9fc8192790e793b7aa9ce084eb771f465c0dc266
ThinulaNimneth <nimnethnetwork@gmail.com>
2026-08-18 11:04:45 +0530

    update

--- src/main/java/lk/ijse/bookstore/entity/Book.java
+++ src/main/java/lk/ijse/bookstore/entity/Book.java
@@ -1,4 +1,17 @@
 package lk.ijse.bookstore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
 }
