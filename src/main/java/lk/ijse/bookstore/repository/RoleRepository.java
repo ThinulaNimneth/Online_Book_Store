9e2bf6415b739f770ff60ac57bdfbeea3fe64b18
ThinulaNimneth <nimnethnetwork@gmail.com>
2026-08-19 20:11:07 +0530

    update

--- src/main/java/lk/ijse/bookstore/repository/RoleRepository.java
+++ src/main/java/lk/ijse/bookstore/repository/RoleRepository.java
@@ -1,4 +1,11 @@
 package lk.ijse.bookstore.repository;
 
-public interface RoleRepository {
+import lk.ijse.bookstore.entity.Role;
+import lk.ijse.bookstore.enumiration.RoleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository  extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(RoleType roleName);
 }
